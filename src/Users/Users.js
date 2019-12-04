55388f68ffdd31bf7854427fc8d34acea8147f58
T.J. Simmons <tj@tjsimmons.net>
2019-12-03 22:58:36 -0600

    done from memory... take that, Mark

---
+++ src/Users/Users.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const Users = function() {
+  const [users, setUsers] = useState([
+    { id: 1, name: "T.J." },
+    { id: 2, name: "Foo" }
+  ]);
+
+  return (
+    <ul>
+      {users.map(u => (
+        <li>{u.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default Users;
